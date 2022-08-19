7f028e29ef10c209d81d0f7ae44926d7738ea569
pythonmaster <theone7877@gmail.com>
2022-08-19 21:09:43 +0300

    test

--- HW_25_fun with yield gen.py
+++ HW_25_fun with yield gen.py
@@ -2,9 +2,8 @@ def add_one(value):
     return value ** 2
 
 def count(start, end, func):
-    for i in range(end):
+    for element in range(end):
         yield start
-        end += 1
         start = func(start)
 
 print(list(count(2, 5, add_one)))
