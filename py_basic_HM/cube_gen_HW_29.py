2c3d6ea5d8954568e74c04a00ca7568ddd06494f
pythonmaster <theone7877@gmail.com>
2022-08-21 21:42:56 +0300

    test

--- py_basic_HM/cube_gen_HW_29.py
+++ py_basic_HM/cube_gen_HW_29.py
@@ -1,6 +1,8 @@
-x = int("input: ")
-y = x >> 1
-if x==(y << 1):
-	print("Наш x - четный")
-else:
-	print("Наш x - нечетный")
+def cube(end):
+	for num in range(2, end):
+		cube = num ** 3
+		if cube < end:
+			yield cube
+
+
+print(list(cube(100)))
--- py_basic_HM/def prime.py
+++ py_basic_HM/def prime.py
@@ -1,9 +1,9 @@
 def add_one(a):
 	if a < 2:
-		return False
+		pass
 	for i in range(2, int(a ** 0.5 + 1)):
 		if a % i==0:
-			return False
+			pass
 	else:
 		return a
 
