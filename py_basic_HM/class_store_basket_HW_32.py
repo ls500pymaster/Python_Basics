ec3ad01166cd5e1a6b746d50ab4ff62293fe9523
pythonmaster <theone7877@gmail.com>
2022-08-28 20:55:04 +0300

    test

--- py_basic_HM/class_store_basket_HW_32.py
+++ py_basic_HM/class_store_basket_HW_32.py
@@ -1,45 +1,49 @@
-class Basket:
-	def __init__(self, name, price, spec, dimension):
-		self._name = name
-		self._price = price
-		self._spec = spec
-		self._dimension = dimension
+class Item:
+
+	def __init__(self, name, price, decription, dimensions):
+		self.price = price
+		self.decription = decription
+		self.dimensions = dimensions
+		self.name = name
 
 	def __str__(self):
-		return f"name = {self._name}, price = {self._price}, spec = {self._spec}, dimension = {self._dimension}"
+		return f"Item = {self.name}, Price = {self.price}, Spec = {self.decription}, Size = {self.dimensions}"
 
 
-class Customer:
-	def __init__(self, name, surname, phone, userid):
-		self._name = name
-		self._surname = surname
-		self._phone = phone
-		self._userid = userid
+class User:
 
-	def __str__(self):
-		return f"Name: {self._name} \n Surname: {self._surname} \n Phone: {self._phone}\n"
-
-
-class Order:
-	def __init__(self, customer):
-		self._products = {}
-		self._customer = customer
-
-	def add_item(self, item, count):
-		count = 0
-		self._products[item] = count
+	def __init__(self, name, surname, numberphone):
+		self.name = name
+		self.surname = surname
+		self.numberphone = numberphone
 
 	def __str__(self):
-		return f"customer = {self._customer}"
+		return f"Name: {self.name} \n Surname: {self.surname} \n Phone: {self.numberphone}\n"
+
+
+class Purchase:
+	def __init__(self, user):
+		self.products = {}
+		self.user = user
+		self.total = 0
+
+	def add_item(self, item, cnt):
+		self.products[item] = cnt
+
+	def __str__(self):
+		return f"Customer: \n {self.user} \n Count: {self.products} \n {self.total}"
 
 	def get_total(self):
-		return f"Customer: \n {self._customer} \n Count: {self._products} \n {self._products.values()} \n {self._products.items()}\n"
+		return self.products.keys(), self.products.values()
 
 
-lemon = Basket("lemon", 5, "yellow", "small", )
-apple = Basket("apple", 10, "green", "medium")
-buyer = Customer("Alexander", "Python", "048100500", "0")
-cart = Order(buyer)
+lemon = Item('lemon', 5, "yellow", "small", )
+apple = Item('apple', 2, "red", "middle", )
+# print(lemon)  # lemon, price: 5
+
+buyer = User("Ivan", "Ivanov", "02628162")
+
+cart = Purchase(buyer)
 cart.add_item(lemon, 2)
 cart.add_item(apple, 3)
 
