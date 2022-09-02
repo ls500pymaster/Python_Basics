2962c5d692231e951ead829a3e3919321b53d08f
pythonmaster <theone7877@gmail.com>
2022-09-02 17:29:09 +0300

    test

--- py_basic_HM/class_store_basket_HW_32.py
+++ py_basic_HM/class_store_basket_HW_32.py
@@ -20,7 +20,6 @@ class User:
 	def __str__(self):
 		return f"Name: {self.name}\nSurname: {self.surname}\nPhone: {self.numberphone}\n"
 
-
 class Purchase:
 	def __init__(self, user):
 		self.products = {}
@@ -30,19 +29,11 @@ class Purchase:
 	def add_item(self, item, cnt):
 		self.products[item] = cnt
 
-	def __str__(self):
-		return f"Customer: \n {self.user} \n Count: {self.products} \n {self.total}"
-
 	def get_total(self):
-		self.total_item = 0
-		for k, v in self.products.items():
-			my_cart = f"{k.name} --> {v}"
-			print(my_cart)
-			self.total_item += v
 		new = []
 		new2 = []
 		self.count = 0
-		keys = self.products.keys()
+		self.total_item = 0
 		for value in self.products.values():
 			new.append(value)
 		for key in self.products.keys():
@@ -50,8 +41,15 @@ class Purchase:
 		tb = [a * b for a, b in zip(new, new2)]
 		for item in tb:
 			self.count += item
-		return f"Total items: {self.total_item}\nTotal price of order: {self.count}\n"
+		return self.count
 
+	def cart_info(self):
+		for k, v in self.products.items():
+			self.my_cart = f"{k.name} --> {v}"
+			print(self.my_cart)
+
+	def __str__(self):
+		return f"Customer: \n {self.user} {self.cart_info()}"
 
 lemon = Item('Lemon', 5, "yellow", "small", )
 apple = Item('Apple', 5, "red", "medium", )
@@ -61,6 +59,6 @@ cart = Purchase(buyer)
 cart.add_item(lemon, 3)
 cart.add_item(apple, 2)
 cart.add_item(rukola, 1)
-print(buyer)
+print(cart)
 full_cart = cart.get_total()
 print(full_cart)
