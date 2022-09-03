93efde80f11aa3ee52e2a8221d4bbc0b30321813
pythonmaster <theone7877@gmail.com>
2022-09-03 22:06:39 +0300

    test

--- py_basic_HM/class_students_HW_33.py
+++ py_basic_HM/class_students_HW_33.py
@@ -20,10 +20,10 @@ class Student(Human):
 		self.record_book = record_book
 
 	def __str__(self):
-		return f"\nName: {self.first_name}\nGender: {self.gender}\nAge: {self.age}"
+		return f"\nName: {self.first_name}\nLast: {self.last_name}\nGender: {self.gender}\nAge: {self.age}"
 
 
-class Group(Student):
+class Group:
 	def __init__(self, number):
 		self.number = number
 		self.group = set()
@@ -33,25 +33,28 @@ class Group(Student):
 
 	def delete_student(self, last_name):
 		res = self.find_student(last_name)
-		pass
+		if res==last_name:
+			self.group.discard(last_name)
 
 	def find_student(self, last_name):
-		param = input("last name: ")
 		for param in self.group:
-			if param==last_name:
-				print(param)
-			else:
-				print("not found")
+			if last_name==param.last_name:
+				return param.last_name
+			return None
 
 	def __str__(self):
-		all_students = ''
-		...
-		return f'Number:{self.number}\n {all_students} '
+		all_students = set()
+		for self.student in self.group:
+			all_students.add(self.student)
+		return f'{self.student}\n'
 
 
 st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st2 = Student('Male', 29, 'Ilon', 'Mask', 'AN143')
 gr = Group('Group Number 1')
 gr.add_student(st1)
-
-gr.find_student('Jobs2')  # None
-# gr.delete_student('Taylor')
+gr.add_student(st2)
+print(gr.find_student("Jobs"))
+print(gr.find_student("Test"))  # None
+print(gr)  # Student
+# gr.delete_student('Jobs')
