7d9c9440274356e9efb0ee414c7f3626bf167e73
pythonmaster <theone7877@gmail.com>
2022-10-11 19:52:11 +0300

    test

--- Telegram_Bot/telegram_bot_main.py
+++ Telegram_Bot/telegram_bot_main.py
@@ -1,1 +1,16 @@
-print("Hello World!")
+from aiogram.utils import executor
+from create_bot import dp
+from handlers import client, admin, other
+from database import sqlite_db
+
+
+async def on_startup(_):
+	print("Bot online")
+	sqlite_db.sql_start()
+
+
+client.register_handlers_client(dp)
+admin.register_handlers_admin(dp)
+other.register_handlers_other(dp)
+
+executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
